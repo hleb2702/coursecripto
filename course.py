2332ac6d3b949a7707469ffb6abfe6b4339cf559
hleb2702 <glebstetko2@gmail.com>
2023-06-25 23:53:16 +0300

    new file

---
+++ course.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+
+def course(valute='bitcoin'):
+    URL = f'https://myfin.by/crypto-rates/{valute}'
+    HEADERS = {
+        'User-Agent': UserAgent().random
+    }
+    response = requests.get(URL, headers=HEADERS)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    items = soup.find('div', class_ = "birzha_info_head_rates").get_text(strip=True)
+    return float(items[:-1])
+
+def valute_list():
+    return [
+        'bitcoin',
+        'ethereum',
+        'tether',
+        'ripple',
+        'bitcoincash',
+        'ethereumclassic',
+        'monero',
+        'dogecoin',
+        'litecoin',
+        'tronix'
+    ]
+
+print(course('ripple'))
+
+
