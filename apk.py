7680d92144b768bf8090c50afaffc427a4f2eb68
hleb2702 <glebstetko2@gmail.com>
2023-06-26 17:08:13 +0300

    ogo v3

--- apk.py
+++ apk.py
@@ -1,18 +1,25 @@
-from course import course, valute_list
-from tkinter import Tk, ttk
+from course import course, valute_list 
+from tkinter import Tk, ttk, font
 
 root = Tk()
 root.title('Course CriptoValute')
-root.geometry('300x250')
+root.geometry('400x300')
+root.resizable(False, False)
+root.configure(bg='white')
+
+myFont = font.Font(family='Arial', size=12)
 
 def go():
     win = Tk()
-    win.title('List')
-    win.geometry('200x200')
+    win.title('List of cryptocurrencies')
+    win.geometry('300x300')
+    win.resizable(False, False)
+    win.configure(bg='white')
+    
     tree = ttk.Treeview(win, show="tree")
     tree.pack(expand=1, fill='both')
 
-    tree.insert("", 'end', iid=1, text="List of criptovalute", open=True)
+    tree.insert("", 'end', iid=1, text="List of cryptocurrencies", open=True)
 
     for i in valute_list:
         a = tree.insert(1, index='end', text=i)
@@ -20,25 +27,25 @@ def go():
 
 def entry():
     try:
-        l2['text'] = f'Price {ent.get()}: {course(ent.get())}$'
+        l2['text'] = f'Price of {ent.get()}: {course(ent.get())}$'
     except:
         l2['text'] = 'Error'
     ent.delete(0, 'end')
 
 
 btn = ttk.Button(text='Get result', command=entry)
-btn2 = ttk.Button(text='Checking list cripto', command=go)
-l1 = ttk.Label(text='Write name\'s cripto:')
-ent = ttk.Entry()
-l2 = ttk.Label(text='')
-l3 = ttk.Label(text='Gleb Hlebnikov was making this GUI :)')
+btn2 = ttk.Button(text='Crypto list', command=go)
+l1 = ttk.Label(text='Enter a cryptocurrency name:', font=myFont)
+ent = ttk.Entry(font=myFont)
+l2 = ttk.Label(text='', font=myFont)
+l3 = ttk.Label(text='Made by Gleb Hlebnikov', font=myFont)
 
-ent.place(x=80, y=50)
-l1.place(x=80, y=30)
-btn.place(x=30, y=150)
-btn2.place(x=150, y=150)
-l2.place(x=80, y=100)
-l3.place(x=50, y=220)
+ent.place(x=120, y=70, width=160, height=30)
+l1.place(x=100, y=40)
+btn.place(x=70, y=150, width=100, height=30)
+btn2.place(x=240, y=150, width=100, height=30)
+l2.place(x=120, y=110)
+l3.place(x=115, y=250)
 
 root.mainloop()
 
